cd0ba7055a91c3f02f7dd13a06c054dc91f7e20e
aeand <60613804+aeand@users.noreply.github.com>
2024-12-01 17:16:59 +0100

    solved day 1 a

--- rust2024/src/main.rs
+++ rust2024/src/main.rs
@@ -1,24 +1,43 @@
 fn main() {
-    let result = std::fs::read_to_string("input.txt").unwrap();
-    let split = result.split("\n");
-    split.clone().for_each(print);
-    let lol = split.map(delete_first);
-    lol.for_each(other_print);
+    day1a();
 }
 
-fn delete_first(input: &str) -> String {
-    if input.is_empty() {
-        return String::new();
+fn day1a() {
+    let result = std::fs::read_to_string("../input/day1.txt").unwrap();
+
+    let input = result
+        .split("\n")
+        .clone();
+
+    let mut list_one = std::vec![];
+    let mut list_two = std::vec![];
+
+    for row in input {
+        let locations: Vec<&str> = row.split("   ").collect();
+
+        let first: i32 = locations.first().unwrap().to_string().parse().unwrap();
+        let second: i32 = locations.last().unwrap().to_string().parse().unwrap();
+
+        list_one.insert(list_one.len(), first);
+        list_two.insert(list_two.len(), second);
     }
 
-    let first_char: char = input.chars().nth(0).unwrap();
-    return input.replace(first_char, "");
-}
+    // sort lists
+    list_one.sort();
+    list_two.sort();
 
-fn print(text: &str) {
-    println!("{text}");
-}
+    let mut distance_counter: i32 = 0;
+    for (index, element) in list_one.iter().enumerate() {
+        // solve puzzle
+        if element > &list_two[index] {
+            distance_counter += element - list_two[index];
+        }
+        else {
+            distance_counter += list_two[index] - element;
+        }
+    }
 
-fn other_print(text: String) {
-    println!("{text}");
+    // 29310557 too high
+    // 1273683 too low
+    // 1579939 right on
 }
