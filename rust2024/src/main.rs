f1a26cded91183c89f19229fcf90be3d325b422d
aeand <60613804+aeand@users.noreply.github.com>
2024-12-01 17:54:09 +0100

    cleaned up day 1

--- rust2024/src/main.rs
+++ rust2024/src/main.rs
@@ -5,49 +5,6 @@ fn main() {
 
 fn day1a() {
     let result = std::fs::read_to_string("../input/day1.txt").unwrap();
-
-    let input = result
-        .split("\n")
-        .clone();
-
-    let mut list_one = std::vec![];
-    let mut list_two = std::vec![];
-
-    for row in input {
-        let locations: Vec<&str> = row.split("   ").collect();
-
-        let first: i32 = locations.first().unwrap().to_string().parse().unwrap();
-        let second: i32 = locations.last().unwrap().to_string().parse().unwrap();
-
-        list_one.insert(list_one.len(), first);
-        list_two.insert(list_two.len(), second);
-    }
-
-    // sort lists
-    list_one.sort();
-    list_two.sort();
-
-    let mut distance_counter: i32 = 0;
-    for (index, element) in list_one.iter().enumerate() {
-        // solve puzzle
-        if element > &list_two[index] {
-            distance_counter += element - list_two[index];
-        }
-        else {
-            distance_counter += list_two[index] - element;
-        }
-    }
-
-    println!("Day 1 A: {}", distance_counter);
-
-    // 29310557 too high
-    // 1273683 too low
-    // 1579939 right on
-}
-
-fn day1b() {
-    let result = std::fs::read_to_string("../input/day1.txt").unwrap();
-
     let input = result
         .split("\n")
         .clone();
@@ -59,11 +16,52 @@ fn day1b() {
         let locations: Vec<&str> = row.split("   ").collect();
         list_one.insert(
             0,
-            locations.first().unwrap().to_string().parse().unwrap()
+            locations.first().unwrap().to_string().parse().unwrap(),
+        );
+        list_two.insert(
+            list_two.len(),
+            locations.last().unwrap().to_string().parse().unwrap(),
+        );
+    }
+
+    list_one.sort();
+    list_two.sort();
+
+    let mut distance: i32 = 0;
+    for (i, el) in list_one.iter().enumerate() {
+        if el > &list_two[i] {
+            distance += el - list_two[i];
+        }
+        else {
+            distance += list_two[i] - el;
+        }
+    }
+
+    println!("Day 1 A: {}", distance);
+
+    // 29310557 too high
+    // 1273683 too low
+    // 1579939 right on
+}
+
+fn day1b() {
+    let result = std::fs::read_to_string("../input/day1.txt").unwrap();
+    let input = result
+        .split("\n")
+        .clone();
+
+    let mut list_one: Vec<i32> = std::vec![];
+    let mut list_two: Vec<i32> = std::vec![];
+
+    for row in input {
+        let locations: Vec<&str> = row.split("   ").collect();
+        list_one.insert(
+            0,
+            locations.first().unwrap().to_string().parse().unwrap(),
         );
         list_two.insert(
             0,
-            locations.last().unwrap().to_string().parse().unwrap()
+            locations.last().unwrap().to_string().parse().unwrap(),
         );
     }
 
