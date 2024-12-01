59d3c7ed37f36ca50790d2cb363f67802a4a7950
aeand <60613804+aeand@users.noreply.github.com>
2024-12-01 17:46:30 +0100

    solved day 2

--- rust2024/src/main.rs
+++ rust2024/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     day1a();
+    day1b();
 }
 
 fn day1a() {
@@ -42,4 +43,43 @@ fn day1a() {
     // 29310557 too high
     // 1273683 too low
     // 1579939 right on
+}
+
+fn day1b() {
+    let result = std::fs::read_to_string("../input/day1.txt").unwrap();
+
+    let input = result
+        .split("\n")
+        .clone();
+
+    let mut list_one: Vec<i32> = std::vec![];
+    let mut list_two: Vec<i32> = std::vec![];
+
+    for row in input {
+        let locations: Vec<&str> = row.split("   ").collect();
+        list_one.insert(
+            0,
+            locations.first().unwrap().to_string().parse().unwrap()
+        );
+        list_two.insert(
+            0,
+            locations.last().unwrap().to_string().parse().unwrap()
+        );
+    }
+
+    let mut similarity_score: i32 = 0;
+    for i in list_one {
+        let mut counter = 0;
+
+        for j in &list_two {
+            if i == *j {
+                counter += 1;
+            }
+        }
+
+        similarity_score += i * counter;
+    }
+
+    println!("Day 1 B: {}", similarity_score);
+    // 20351745 right on
 }
