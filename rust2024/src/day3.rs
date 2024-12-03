61bdaccf826f48caa7817910c1d3006349ba5903
aeand <aeand2@gmail.com>
2024-12-03 21:09:23 +0100

    cleaned up day 3 a

--- rust2024/src/day3.rs
+++ rust2024/src/day3.rs
@@ -1,24 +1,17 @@
 use regex::Regex;
 
 pub fn day3a() {
-  let result = std::fs::read_to_string("/home/antan/Projects/advent-of-code/rust2024/input/day3.txt").unwrap();
-
-  let re = Regex::new(r"mul\([\d]+,[\d]+\)");
-  if re.is_err() {
-    println!("fuck")
-  }
-
-  let apa = re.unwrap();
-
-  let res = apa.find_iter(&result);
+  let input = std::fs::read_to_string("/home/antan/Projects/advent-of-code/rust2024/input/day3.txt").unwrap();
+  let re = Regex::new(r"mul\([\d]+,[\d]+\)").unwrap();
+  let regex = re.find_iter(&input);
 
   let mut score = 0;
-  for r in res {
+  for r in regex {
     let v: Vec<&str> = r.as_str().split(",").clone().collect();
 
-    let a: i32 = v[0].replace("mul(", "").parse().unwrap();
-    let b: i32 = v[1].replace(")", "").parse().unwrap();
-    score += a * b;
+    score +=
+      v[0].replace("mul(", "").parse::<i32>().unwrap()
+      * v[1].replace(")", "").parse::<i32>().unwrap();
   }
 
   // 175015740 right on
