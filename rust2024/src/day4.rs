0e7e1acff0e06bcf05070bc45354684264eb8d24
aeand <aeand2@gmail.com>
2024-12-07 00:16:11 +0100

    started day 4 b

--- rust2024/src/day4.rs
+++ rust2024/src/day4.rs
@@ -84,10 +84,10 @@ fn check_top_diagonal(input: Vec<&str>, size: usize) -> String {
 }
 
 fn check_bottom_diagonal(input: Vec<&str>, size: usize) -> String {
-  let mut result: String = "".to_string();
+  let mut all_diagonals: String = "".to_string();
 
   for x in 1..size {
-    let mut stri = "".to_string();
+    let mut diagonal = "".to_string();
 
     for y in 0..size {
       if y > size-1 || x+y > size-1 {
@@ -95,40 +95,87 @@ fn check_bottom_diagonal(input: Vec<&str>, size: usize) -> String {
       }
 
       let ch: Vec<char> = input[y].chars().collect();
-      stri = format!("{}{}", stri, ch[x+y].to_string());
+      diagonal = format!("{}{}", diagonal, ch[x+y].to_string());
     }
 
-    if stri.len() >= 4 {
-      stri = format!("{}{}", stri, "\n");
-      result = format!("{}{}", result, stri);
+    if diagonal.len() >= 4 {
+      diagonal = format!("{}{}", diagonal, "\n");
+      all_diagonals = format!("{}{}", all_diagonals, diagonal);
     }
   }
 
-  for x in 0..size-1 {
-    let mut stri = "".to_string();
+  for x in 0..size {
+    let mut diagonal = "".to_string();
     for y in 0..x+1 {
       if x-y > x {
         break;
       }
 
-      let ch: Vec<char> = input[y].chars().collect();
-      stri = format!("{}{}", stri, ch[x-y].to_string());
+      let chars: Vec<char> = input[y].chars().collect();
+      diagonal = format!("{}{}", diagonal, chars[x-y].to_string());
     }
 
-    if stri.len() >= 4 {
-      stri = format!("{}{}", stri, "\n");
-      result = format!("{}{}", result, stri);
+    if diagonal.len() >= 4 {
+      diagonal = format!("{}{}", diagonal, "\n");
+      all_diagonals = format!("{}{}", all_diagonals, diagonal);
     }
   }
 
-  return result;
+  return all_diagonals;
 }
 
 pub fn day4b() {
   let input = std::fs::read_to_string("/home/antan/Projects/advent-of-code/rust2024/input/day4.txt").unwrap();
 
+  let mut rows: Vec<&str> = input.split("\n").collect();
+  let size: usize = rows.len();
+  let mut all_diagonals: String = "".to_string();
+
   // get diagonals in one direction
+  for x in 0..size {
+    let mut diagonal = "".to_string();
+
+    for y in 0..size {
+      if y > size-1 || x+y > size-1 {
+        break;
+      }
+
+      let chars: Vec<char> = rows[y].chars().collect();
+      diagonal = format!("{}{}", diagonal, chars[x+y].to_string());
+    }
+
+    if diagonal.len() >= 4 {
+      diagonal = format!("{}{}", diagonal, "\n");
+      all_diagonals = format!("{}{}", all_diagonals, diagonal);
+    }
+  }
+
+  rows.reverse();
+
+  for x in 0..size {
+    let mut diagonal = "".to_string();
+    for y in 0..x+1 {
+      if x-y > x {
+        break;
+      }
+
+      let chars: Vec<char> = rows[y].chars().collect();
+      diagonal = format!("{}{}", diagonal, chars[x-y].to_string());
+    }
+
+    if diagonal.len() >= 4 {
+      diagonal = format!("{}{}", diagonal, "\n");
+      all_diagonals = format!("{}{}", all_diagonals, diagonal);
+    }
+  }
+
+  for a in all_diagonals.split("\n") {
+      println!("{a}");
+  }
+
   // find indexes of mas
+  let indexes = all_diagonals.match_indices("mas");
+
   // take the 'a' index
   // find 3 char string on that index. aka the diagonal in the other direction over 'a'
   // find match in that string on the same index as previous match
